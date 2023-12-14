e23618437aafabd512f20b0f898e7b588671e897
Samruddhi24 <patilsamruddhi690@gmail.com>
2023-12-14 22:12:01 +0530

    added number.js inbuilt functionalities

---
+++ Week 1/week_1offline_class/number.js
@@ -0,0 +1,11 @@
+let a="ppp42pppp"
+
+console.log(typeof(a))
+let b = parseInt(a)
+console.log(typeof(b))
+//this will print NaN BUT the type is number 
+console.log (b)
+
+let cs=4
+let c =parseFloat(a)
+console.log(c)

39e39d615127041cf9802a7cc0eb02e9a5ff41e0
Samruddhi24 <patilsamruddhi690@gmail.com>
2023-12-26 17:56:43 +0530

    added code to catch the query parameter

--- Week 2/http_server/index.js
+++ Week 2/http_server/index.js
@@ -1,12 +1,30 @@
 const express=require("express")
 const app = express()
 const port =3000
-// function get_req(req,res)
-// {
-//     res.send("hello world")
-// }
-//app.get('/',get_req)
+//basic http server
+/*function get_req(req,res)
+ {
+     res.send("hello world")
+ }
+app.get('/',get_req)
 app.get('/',function(req,res){
     res.send("hello world")
 })
-app.listen(port)
+app.listen(port)*/
+
+// adding query parameter
+function sum(n)
+{
+    let ans =0
+    for (let i=0; i<n;i++)
+    {
+        ans +=i;
+    }
+    return ans;
+}
+app.get("/",function(req,res){
+    const n=req.query.n
+    const ans =sum(n)
+    res.send("Sum of "+n+"number is "+ans)
+})
+app.listen(port);

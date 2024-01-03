ca19df90dd9776dc1a2c60d04a68bcdae2ba30c9
Samruddhi24 <patilsamruddhi690@gmail.com>
2024-01-04 00:09:41 +0530

    prechecks using middleware

--- Week 3/Middlewares/index.js
+++ Week 3/Middlewares/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 //basic code that do authentication by if and else using headers
-app.get("/kidney",function(req, res){
+/*app.get("/kidney",function(req, res){
     let kidneyid=req.query.n
     let username =req.headers.username
     let password =req.headers.password
@@ -33,5 +33,38 @@ app.get("/kidney",function(req, res){
     }
     res.json({msg: "everthing is all right"})
     
+})*/
+
+//now for every route we need do authentication and so the code will be repeated 
+// we can also use function to avoid this repeatation but still there is repeated
+//we use middleware
+//here authcheck and idcheck are doing job of middleware 
+
+function authcheck(req,res,next)
+{
+    let username =req.headers.username
+    let password =req.headers.password
+    if (username == "sam" && password =="pass")
+    {       
+        next()      
+    }
+    else{
+        res.status(400).json({
+            msg:"authentication Fail"
+        }) 
+    }
+}
+function idcheck(req,res,next){
+    let kidneyid=req.query.n
+    if (kidneyid ==1)
+    {
+        next()
+    }
+    else{
+        res.status(400).json({msg:"Not ok"})
+    }
+}
+app.get("/chekup",authcheck,idcheck,function(req,res){
+    res.json({meg:"success"})
 })
 app.listen(3000)

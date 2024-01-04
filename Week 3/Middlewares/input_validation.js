bb5c681fe57bbce555e653099628e8b991a63a84
Samruddhi24 <patilsamruddhi690@gmail.com>
2024-01-04 22:38:31 +0530

    added zod lecture code

--- Week 3/Middlewares/input_validation.js
+++ Week 3/Middlewares/input_validation.js
@@ -1,5 +1,6 @@
 const express =require ("express")
 const app =express()
+const zod= require("zod")
 app.use(express.json())
 //here we are expecting only array input 
 //if other send then they can see through our server 
@@ -7,7 +8,7 @@ app.use(express.json())
 // we can do using if else but there are better ways to do it 
 //we can use global catches
 //every time if exception came then it uses global catches 
-app.post("/",(req,res)=>{
+/*app.post("/",(req,res)=>{
     const kidney=req.body.kidneys;
     const kidney_length=kidney.length;
     res.send("length is "+ kidney_length)
@@ -18,5 +19,18 @@ app.use(function(err,req,res,next)
 res.json({
     msg:"error"
 })
+})*/
+
+//we can do input validation using zod 
+//this only handle the input validation
+//this do not exception
+//it is not mandatory to use zod with the http servers only we can use them in the normal code also to validate input 
+const schema =zod.array(zod.number());
+app.post("/",(req,res)=>{
+    const kidney=req.body.kidneys;
+    const kidney_length=kidney.length;
+    let response =schema.safeParse(kidney)
+    res.send(response)
 })
+
 app.listen(3000)

15275d62cefc4cc881143e2c794884095fd72546
Samruddhhi24 <153808435+Samruddhhi24@users.noreply.github.com>
2024-01-10 23:07:31 +0530

    added http post with mongo db

--- Week 3/3.2/authUsingMongo.js
+++ Week 3/3.2/authUsingMongo.js
@@ -1,12 +1,39 @@
 
 const mongoose=require("mongoose")
+const express=require("express")
+const app = express()
+app.use(express.json())
+
 mongoose.connect('mongodb+srv://admin:8bXLVZnvCKp928G@atlascluster.wljsgf7.mongodb.net/')
-const User = mongoose.model('users',
-{ name : String ,
-    email_id: String ,
-    password : String})
-const user = new User({name : "sam",
- email_id:"patil@gmail.com",
- password:"121434"})
+const User = mongoose.model('users',{ name : String ,email_id: String ,password : String})
+app.post("/signup",async (req,res)=>{
+    const username = req.body.username
+    const password =req.body.password
+    const name =req.body.name
+    const userexists = await User.findOne({email_id:username})
+    if(userexists)
+    {
+        res.status(400).json({
+            msg:"user already exists"
+        })
+    
+        
+    }
+    else{
+        const user = new User({name : name,
+            email_id:username,
+            password:password})
+            user.save()
+            res.status(200).json({
+                msg:"user sign up successfull"
+            })
+        }
+        
+
+    
+
+})
+
  
-user.save()
+
+app.listen(3000)

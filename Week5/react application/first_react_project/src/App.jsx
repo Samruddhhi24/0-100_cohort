c5817f7b09908aa9b5c89d70eb9841f157b8b130
Samruddhhi24 <153808435+Samruddhhi24@users.noreply.github.com>
2024-02-10 20:32:04 +0530

    todo react app

--- Week5/react application/first_react_project/src/App.jsx
+++ Week5/react application/first_react_project/src/App.jsx
@@ -4,25 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-  const [count,inc]=useState(0)
- 
-  return (
-    
-      <div>
-       <Custombutton count={count} inc={inc}></Custombutton>
-       
-       
-      </div>)
+  const [todos, setTodos] = useState([{
+    title: "Go to gym",
+    description: "Go to gym from 7-9",
+    completed: false
+  }, {
+    title: "Study DSA",
+    description: "Study DSA form 9-100",
+    completed: true
+  }, {
+    title: "Study DSA",
+    description: "Study DSA form 9-100",
+    completed: true
+  }, ]); 
 
-      
-}
-function Custombutton(props){
-  function onclicklistner(){
-    props.inc(props.count+1)
+  function addTodo() {
     
+    setTodos([...todos, {
+      title: "new Todo",
+      description: "desc of new todo"
+    }])
   }
-  return <button onClick={onclicklistner}>counter {props.count}</button>
+
+  return (
+    <div>
+      <button onClick={addTodo}>Add a random todo</button>
+      {todos.map(function(todo) {
+        return <Todo title={todo.title} description={todo.description} />
+      })}
+
+    </div>
+  )
+}
+
+function Todo(props) {
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
 }
 
 export default App
